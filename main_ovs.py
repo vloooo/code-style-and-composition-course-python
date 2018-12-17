4e733424050991475571aa211ef2419eabb0a78a
vloooo <vlad.m.o12@gmail.com>
2018-12-17 10:49:04 +0200

    new PR

--- main_ovs.py
+++ main_ovs.py
@@ -5,47 +5,36 @@ import string
 import json
 
 
-def read_file(name):
-    """ функция для чтения из файла """
-    with open(name) as fl:
-        return fl.readlines()
+class FileReader:
+    @staticmethod
+    def read_file(name):
+        """ func to read from file """
+        with open(name) as fl:
+            return fl.readlines()
 
 
-class LineHandler:
-    """ класс для выполнения задания по обработке"""
+class JsonWriter:
+    @staticmethod
+    def write_json(name, jsn):
+        with open(name, 'w') as file:                   # serialization
+            json.dump(jsn, file)
 
-    def __init__(self, line):
-        self.inf_dict = {}
-        self.line = line                                                    # входная строка
-        self.inf_dict['body'] = ''                                          # словарь содержащий требуемые поля
-        self.inf_dict["metadata"] = []
-        self.inf_dict['body_tags'] = []
-        self.inf_dict['orphan_tokens'] = []
 
-    def explore(self):
+class RemoverUnwantedWords:
+    @staticmethod
+    def remove_words(expr, line):
+        """ func for cleaning by regex """
+        return re.sub(expr, '', line)
 
-        dog_index = self.line.rfind('@') + 1
-        self.inf_dict["metadata"].append(self.line[dog_index:-3])            # нахожу [@...]
 
-        self.line = self.line[:dog_index-4]
-        self.inf_dict['body'] = self.line                                    # заполняю body
-        clean_line = self.remove_words(r'[\$]\w+', self.line)                # удаляю долларовые слова
-
-        self.inf_dict['body_tags'] = re.findall(r'[\#\@](\w+)', clean_line)  # зплняю body_tags очищаю строку
-        clean_line = self.remove_words(r'[\#\@](\w+)', clean_line)
-
-        clean_line = self.find_urls(clean_line)
-
-        self.find_trash(clean_line)
-
-        return self.inf_dict
-
-    def find_trash(self, clean_line):
-        """ поиск безсмысленых слов """
+class CheckerForMeaning:
+    @staticmethod
+    def find_trash(clean_line, inf_dict):
+        """ find contextless word """
         tokens = clean_line.split(' ')
 
         clean_tokens = []
-        for i in range(len(tokens)):                                           # очстка от "" и пунктуации (односимвлн слова нам тоже не нужны)
+        for i in range(len(tokens)):                    # removing punctuation and " "
             if len(tokens[i]) < 2:
                 continue
             while True:
@@ -56,34 +45,60 @@ class LineHandler:
                     break
 
         clean_tokens = set(clean_tokens)
-        for i in clean_tokens:                                                 # проверка на смсл
+        for i in clean_tokens:                          # check for meaning
             if nltk.wsd.lesk(tokens, i) is None:
-                self.inf_dict['orphan_tokens'].append(i)
+                inf_dict['orphan_tokens'].append(i)
+        return inf_dict
 
-    def find_urls(self, clean_line):
-        """ отлавливание ЮРЛОВ """
-        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', self.line)
-        clean_line = self.remove_words('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
-                                       clean_line)
+
+class UrlFinder:
+    @staticmethod
+    def find_urls(clean_line, raw_line, inf_dict):
+        """ find URL """
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', raw_line)
+        funk_for_clean = RemoverUnwantedWords.remove_words
+        clean_line = funk_for_clean('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+                                    clean_line)
 
         if len(urls):
-            self.inf_dict["metadata"].extend(urls)
-        return clean_line
+            inf_dict["metadata"].extend(urls)
+        return clean_line, inf_dict
 
-    def remove_words(self, expr, line):
-        """ функция для очисти регулярочками """
-        return re.sub(expr, '', line)
+
+class TweetLineHandler:
+    """ class for performance extraction useful inf from tweet"""
+
+    def __init__(self, line):
+        self.line = line
+
+    def explore(self):
+        inf_dict = {'body': '', "metadata": [], 'body_tags': [], 'orphan_tokens': []}
+
+        dog_index = self.line.rfind('@') + 1
+        inf_dict["metadata"].append(self.line[dog_index:-3])                        # find [@...]
+
+        line = self.line[:dog_index - 4]
+        inf_dict['body'] = line
+        clean_line = RemoverUnwantedWords.remove_words(r'[\$]\w+', line)            # remove $-words
+
+        inf_dict['body_tags'] = re.findall(r'[\#\@](\w+)', clean_line)
+        clean_line = RemoverUnwantedWords.remove_words(r'[\#\@](\w+)', clean_line)  # clean line
+
+        clean_line, inf_dict = UrlFinder.find_urls(clean_line, self.line, inf_dict)
+
+        inf_dict = CheckerForMeaning.find_trash(clean_line, inf_dict)
+
+        return inf_dict
 
 
 if __name__ == "__main__":
 
-    data = read_file('input.txt')                   # чтение из файла
+    data = FileReader.read_file('input.txt')  # reading from file
 
-    result = {'records': []}                        # словарь для сериализации
+    result = {'records': []}
 
-    for i in data:                                  # обработка строк
-        hndlr = LineHandler(i)
-        result['records'].append(hndlr.explore())
+    for i in data:                            # tweet handling
+        hndlr = TweetLineHandler(i)
+        result['records'].append(hndlr.explore)
 
-    with open('output.json', 'w') as file:          # сереализация
-        json.dump(result, file)
+    JsonWriter.write_json('output.json', result)
